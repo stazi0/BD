5c04a9626747c055d4d1e02ac57d3b461b365559
ipz <fahaka6359@klanze.com>
2023-12-09 13:01:31 +0200

    first commit

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.android.unscramble.ui.game
+
+class GameViewModel {
+    fun readProfiles() {
+        TODO("Not yet implemented")
+    }
+
+    fun saveProfile() {
+        TODO("Not yet implemented")
+    }
+
+    private val viewModel: GameViewModel by viewModels()
---
+++ app/src/main/java/com/example/android/unscramble/ui/game/Profile.kt
@@ -0,0 +1,7 @@
+package com.example.android.unscramble.ui.game
+
+class Profile(
+    val name: String,
+    val email: String
+
+)
